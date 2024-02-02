efe23406a5d6287735490848ba4b5a8824b4b5c1
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-03 02:14:27 +0300

    Added React Router to the project.

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <RouterProvider router={router}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </RouterProvider>
   </React.StrictMode>
 );
