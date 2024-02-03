d021c750ba2df4cc9906bd0c073491133293d662
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-03 05:12:39 +0300

    Update EditWordForm component to use Redux and add PropTypes

--- src/components/EditWordForm.jsx
+++ src/components/EditWordForm.jsx
@@ -1,34 +1,60 @@
+import { useSelector, useDispatch } from "react-redux";
 import Input from "./Input";
 import EditButton from "./buttons/EditButton";
 import DeleteButton from "./buttons/DeleteButton";
 import AddSentenceButton from "./buttons/AddSentenceButton";
+import { editWord, selectWordById } from "../redux/words/wordsSlice";
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-function EditWordForm() {
+function EditWordForm({ id }) {
+  const data = useSelector((state) => selectWordById(state, id));
+  const [word, setWord] = useState(data.word);
+  const [meaning, setMeaning] = useState(data.meaning);
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(editWord({ id, changes: { word, meaning } }));
+  };
+
   return (
-    <div className="py-4 px-8 bg-slate-700 rounded-lg">
-      <h2 className="text-center font-semibold text-xl p-2">Add a new word</h2>
+    <div className="py-4 px-8 bg-slate-700 rounded-lg w">
+      <h2 className="text-center font-semibold text-xl p-2"></h2>
 
-      <form onSubmit={null} className="flex items-start">
-        <div className="flex flex-col">
-          <Input required />
+      <form onSubmit={handleSubmit} className="flex items-start">
+        <div className="flex flex-col flex-1">
+          <Input
+            onChange={(e) => setWord(e.target.value)}
+            value={word}
+            required
+          />
           <span className="font-extralight text-sm text-slate-400 mt-1">
             Word
           </span>
         </div>
 
-        <div className="flex flex-col">
-          <Input required />
+        <div className="flex flex-col flex-1">
+          <Input
+            onChange={(e) => setMeaning(e.target.value)}
+            value={meaning}
+            required
+          />
           <span className="font-extralight text-sm text-slate-400 mt-1">
             Meaning
           </span>
         </div>
 
-        <EditButton />
-        <DeleteButton />
-        <AddSentenceButton />
+        <EditButton type="submit" />
+        <DeleteButton type="button" />
+        <AddSentenceButton type="button" />
       </form>
     </div>
   );
 }
+EditWordForm.propTypes = {
+  id: PropTypes.string.isRequired,
+};
 
 export default EditWordForm;
