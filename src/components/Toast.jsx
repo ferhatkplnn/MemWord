e780fa66952bc2bd3fe7f9bef62158a3cf66a868
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-04 01:24:29 +0300

    Disable eslint rule for missing dependency in `useEffect` hook.

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -26,6 +26,7 @@ function Toast() {
       clearInterval(id1);
       clearInterval(id2);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleCloseToast = () => {
