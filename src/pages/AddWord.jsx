0ef3963e5f9b8c0eba7cba338f46309729ff1ac7
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-03 04:04:32 +0300

    Add AddWord page and route

--- src/pages/AddWord.jsx
+++ src/pages/AddWord.jsx
@@ -1,5 +1,15 @@
+import AddWordForm from "../components/AddWordForm";
+
 function AddWord() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-medium p-8">Add Word</h1>
+
+      <AddWordForm />
+
+      <div></div>
+    </div>
+  );
 }
 
 export default AddWord;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import AddWord from "../pages/AddWord";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/add-word",
+    element: <AddWord />,
   },
 ]);
