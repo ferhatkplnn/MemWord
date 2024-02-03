72b5484341cebae4bec060d4e9c827b2f454b5ee
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-03 07:10:19 +0300

    Added sentence modal and UI slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import wordsReducer from "./words/wordsSlice";
+import UIReducer from "./words/UISlice";
 
 export const store = configureStore({
   reducer: {
     words: wordsReducer,
+    UI: UIReducer,
   },
 });
