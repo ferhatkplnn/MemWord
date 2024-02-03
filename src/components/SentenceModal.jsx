b8d3a338789a406d966c28f5f4ccdcfb1b5240d8
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-03 07:10:50 +0300

    Add sentence modal and functionality

--- src/components/SentenceModal.jsx
+++ src/components/SentenceModal.jsx
@@ -1,23 +1,71 @@
+import { useEffect, useState } from "react";
+import { toggleModal } from "../redux/words/UISlice";
 import Input from "./Input";
+import { useDispatch, useSelector } from "react-redux";
+import { addSentence, selectWordById } from "../redux/words/wordsSlice";
+function SentenceModal({ id }) {
+  const [sentence, setSentence] = useState("");
+  const { sentences } = useSelector((state) => selectWordById(state, id));
+  const [isEffectActive, setIsEffetcActive] = useState(false);
 
-function SentenceModal() {
+  const dispatch = useDispatch();
+  const handleCloseModal = () => {
+    dispatch(toggleModal());
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      addSentence({ id, changes: { sentences: [...sentences, sentence] } })
+    );
+    setSentence("");
+    handleCloseModal();
+  };
+
+  useEffect(() => {
+    setIsEffetcActive(true);
+    console.log("Modal render edildi");
+    return () => {
+      console.log("Modla sokuldu");
+    };
+  }, []);
+
   return (
     <>
-      <div className="absolute w-full h-screen inset-0 bg-black/80 flex justify-center items-center duration-500">
-        <div className="relative flex-1 max-w-xl flex flex-col bg-slate-800 space-y-8 rounded-md p-8">
+      <div className="fixed w-full h-screen inset-0 backdrop-blur-lg flex justify-center items-center">
+        <div
+          className={`relative opacity-70 scale-110 flex-1 max-w-xl flex flex-col bg-slate-800 space-y-8 rounded-md p-8 duration-500 ${
+            isEffectActive ? " !opacity-100 !scale-100" : ""
+          }`}
+        >
           <h3 className="text-xl font-bold">Add sentence</h3>
-          <Input
-            placeholder="Add sentence"
-            className="placeholder:text-slate-600"
-          />
+          <form onSubmit={handleSubmit}>
+            <Input
+              autoFocus
+              required
+              value={sentence}
+              onChange={(e) => setSentence(e.target.value)}
+              placeholder="Add sentence"
+              className="placeholder:text-slate-600 w-full"
+            />
+          </form>
           <div className="flex justify-end space-x-2">
-            <button className="bg-sky-300/90 text-slate-800 font-semibold px-4 py-2 rounded-md hover:bg-sky-300/70">
+            <button
+              onClick={handleSubmit}
+              className="bg-sky-300/90 text-slate-800 font-semibold px-4 py-2 rounded-md hover:bg-sky-300/70"
+            >
               Save
             </button>
-            <button className="bg-slate-700  font-semibold px-4 py-2 rounded-md hover:bg-slate-600">
+            <button
+              onClick={handleCloseModal}
+              className="bg-slate-700  font-semibold px-4 py-2 rounded-md hover:bg-slate-600"
+            >
               Cancel
             </button>
-            <button className="absolute right-2 top-2 p-1 rounded-md hover:bg-slate-700 duration-150">
+            <button
+              onClick={handleCloseModal}
+              className="absolute right-2 top-2 p-1 rounded-md hover:bg-slate-700 duration-150"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
