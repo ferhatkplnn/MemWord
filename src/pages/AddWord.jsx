7a5480d936c8a34e00d39027b535de11541c47d4
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-03 04:46:32 +0300

    Add EditWordForm component to AddWord page

--- src/pages/AddWord.jsx
+++ src/pages/AddWord.jsx
@@ -1,13 +1,21 @@
 import AddWordForm from "../components/AddWordForm";
-
+import { useSelector } from "react-redux";
+import { selectWordIds } from "../redux/words/wordsSlice";
+import EditWordForm from "../components/EditWordForm";
 function AddWord() {
+  const wordIds = useSelector(selectWordIds);
+  console.log(wordIds);
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center space-y-20">
       <h1 className="text-4xl font-medium p-8">Add Word</h1>
 
       <AddWordForm />
 
-      <div></div>
+      <div>
+        {wordIds.map((id) => (
+          <EditWordForm key={id} />
+        ))}
+      </div>
     </div>
   );
 }
