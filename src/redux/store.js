f7beccba3c90d6cf851d60ecf14942cfb9d327d2
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-04 01:48:59 +0300

    Add listener middleware to save words.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createListenerMiddleware } from "@reduxjs/toolkit";
 import wordsReducer from "./words/wordsSlice";
 import UIReducer from "./words/UISlice";
+import { saveWords } from "./listeners";
+
+const listenerMiddleware = createListenerMiddleware();
+listenerMiddleware.startListening(saveWords);
 
 export const store = configureStore({
   reducer: {
     words: wordsReducer,
     UI: UIReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(listenerMiddleware.middleware),
 });
