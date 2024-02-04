cc0e1b5cf6c23005aeb5aca72273eaa3fe0dec4c
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-04 13:12:59 +0300

    Added function to hide letters in a word and used it in Box1 component.

--- src/pages/Box1.jsx
+++ src/pages/Box1.jsx
@@ -1,13 +1,16 @@
 import { useSelector } from "react-redux";
 import { selectBox1Words } from "../redux/words/wordsSlice";
 import Input from "../components/Input";
-import { getRandomWord } from "../utils/utils";
+import { getRandomWord, hideWordLetters } from "../utils/utils";
 import { useState } from "react";
 
 function Box1() {
   const [inputText, setInputText] = useState("");
   const words = useSelector(selectBox1Words);
   const [randomWord, setRandomWord] = useState(getRandomWord(words));
+  const [hiddenWord, setHiddenWord] = useState(
+    hideWordLetters(randomWord.word)
+  );
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,7 +18,7 @@ function Box1() {
     if (randomWord.word.toLocaleLowerCase() === inputText.toLocaleLowerCase()) {
       alert("Correct!");
     } else {
-      //
+      setHiddenWord(randomWord.word);
     }
   };
 
@@ -24,24 +27,24 @@ function Box1() {
       <h2 className="text-3xl font-semibold">Beginner Word Box</h2>
       <div className="flex flex-col items-center p-4 rounded-md bg-slate-700 w-1/3 min-w-96">
         <div className="text-2xl font-semibold">{randomWord.meaning}</div>
-        <div className="text-4xl font-extrabold h-40 break-all mt-12 ">
-          {randomWord.word}
+        <div className="text-4xl font-extrabold h-40 break-all mt-12 tracking-widest">
+          {hiddenWord}
         </div>
         <div className="text-sm text-slate-400 text-center">
           this is a pencil
         </div>
         <div className="flex flex-col w-full space-y-2">
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit} className="space-y-2">
             <Input
               className="w-full"
               value={inputText}
               onChange={(e) => setInputText(e.target.value)}
             />
-          </form>
 
-          <button className="bg-sky-300 text-slate-800 font-bold py-2 rounded-md hover:bg-sky-400 duration-150">
-            Send
-          </button>
+            <button className="w-full bg-sky-300 text-slate-800 font-bold py-2 rounded-md hover:bg-sky-400 duration-150">
+              Send
+            </button>
+          </form>
         </div>
       </div>
     </div>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,3 +3,26 @@ export const getRandomWord = (words) => {
   const randomWord = words[randomIndex];
   return randomWord;
 };
+
+export const hideWordLetters = (word) => {
+  if (typeof word !== "string") {
+    throw new Error("Input should be a string.");
+  }
+  const length = word.length;
+
+  if (length <= 1) {
+    return word;
+  }
+
+  let indices = Array.from({ length: 2 }, () =>
+    Math.floor(Math.random() * length)
+  );
+
+  while (indices[0] === indices[1]) {
+    indices[1] = Math.floor(Math.random() * length);
+  }
+
+  return Array.from({ length }, (_, i) =>
+    indices.includes(i) ? word[i] : "-"
+  ).join("");
+};
