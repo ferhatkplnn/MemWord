31c124bfe8e167b59e17f5789bb04ae80f9dc5fc
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-04 18:00:46 +0300

    Update the design of the home page buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,16 @@ function Home() {
       <div className="flex justify-center items-center">
         <div className="grid grid-cols-2 gap-2">
           <Link to="/add-word">
-            <div className="border-2 p-8 text-center space-y-4">
+            <div className="bg-slate-700 rounded-lg shadow-xl p-8 text-center space-y-4 hover:ring-4">
               <h2 className="text-4xl font-medium">Add Word</h2>
-              <p className="font-light text-slate-500">
+              <p className="font-light  text-slate-500">
                 You can add words from here.
               </p>
             </div>
           </Link>
 
           <Link to={"/box-1"}>
-            <div className="border-2 p-8 text-center space-y-4">
+            <div className="bg-slate-700 rounded-lg shadow-xl p-8 text-center space-y-4 hover:ring-4">
               <h2 className="text-4xl font-medium">Box 1</h2>
               <p className="font-light text-slate-500">
                 This box contains the first words you added.
