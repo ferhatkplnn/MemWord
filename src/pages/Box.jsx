fee3ff5b78e6c03eaeac1cb468e640140a9b9d80
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-11 19:42:10 +0300

    Add word count and score to the box component.

--- src/pages/Box.jsx
+++ src/pages/Box.jsx
@@ -73,8 +73,37 @@ function Box({ selectBoxWords, decreaseAmount, isShowHiddenWord = false }) {
         className="drop-shadow-2xl flex flex-col items-center p-4 rounded-md bg-slate-700 w-11/12 sm:w-2/3 lg:w-1/3"
       >
         <div className="flex w-full justify-between">
-          <SpeakButton className="" text={word} />{" "}
-          <span className="font-bold">{words.length}</span>{" "}
+          <SpeakButton className="" text={word} />
+          <div className="flex">
+            <span className="font-bold">{words.length}</span>
+            <i className="group relative cursor-pointer">
+              <div className="absolute hidden group-hover:block drop-shadow-lg rounded-lg right-0 top-0 translate-x-full -translate-y-full px-4 py-2 bg-slate-700 w-32">
+                <div className="flex justify-between">
+                  Score: <span>{randomWord.count.score}</span>
+                </div>
+                <div className="flex justify-between">
+                  Current: <span>{randomWord.count.current}</span>
+                </div>
+                <div className="flex justify-between">
+                  Wrong: <span>{randomWord.count.wrong}</span>
+                </div>
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+                />
+              </svg>
+            </i>
+          </div>
         </div>
         <div className="text-2xl font-semibold">{meaning}</div>
         <div
