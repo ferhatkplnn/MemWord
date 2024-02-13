655bae754e46cb2fe770dbf2e3656156b8f1bde5
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-13 15:41:17 +0300

    Replace ScoreBoard component with WordInformation component in Box component

--- src/pages/Box.jsx
+++ src/pages/Box.jsx
@@ -15,7 +15,7 @@ import {
 import { useEffect, useRef, useState } from "react";
 import SpeakButton from "../components/SpeakButton";
 import ProgressBar from "../components/ProgressBar";
-import ScoreBoard from "../components/ScoreBoard";
+import WordInformation from "../components/WordInformation";
 
 function Box({ selectBoxWords, decreaseAmount, isShowHiddenWord = false }) {
   const dispatch = useDispatch();
@@ -76,26 +76,7 @@ function Box({ selectBoxWords, decreaseAmount, isShowHiddenWord = false }) {
       >
         <div className="flex w-full justify-between">
           <SpeakButton className="" text={word} />
-          <div className="flex">
-            <span className="font-bold">{words.length}</span>
-            <i className="group relative cursor-pointer">
-              <ScoreBoard randomWord={randomWord} />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
-                />
-              </svg>
-            </i>
-          </div>
+          <WordInformation words={words} randomWord={randomWord} />
         </div>
         <div className="text-2xl font-semibold">{meaning}</div>
         <div
