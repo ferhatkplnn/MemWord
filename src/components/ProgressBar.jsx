24352128e9a3360c5232a05bdbb5760e62a182a1
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-14 13:08:24 +0300

    Change progress bar width and height

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -5,7 +5,7 @@ function ProgressBar({ score }) {
   const color = colorByPercentage(scorePercentage);
 
   return (
-    <div className="w-48 h-4 border border-slate-500 rounded-lg overflow-hidden">
+    <div className="w-40 h-3 bg-slate-600 border-slate-500 rounded-lg overflow-hidden">
       <div
         style={{ width: `${scorePercentage}%` }}
         className={`${color} h-full  duration-700 ease-in`}
