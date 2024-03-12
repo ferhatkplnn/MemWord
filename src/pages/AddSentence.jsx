441545906c8ee3ba6a9d95640a9b6bdeb06329e6
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-12 12:03:52 +0300

    feat: Integrate AddSentenceForm into AddSentence component
    - Updated AddSentence component to include AddSentenceForm
    - Improved styling for a more user-friendly interface

--- src/pages/AddSentence.jsx
+++ src/pages/AddSentence.jsx
@@ -1,5 +1,14 @@
+import AddSentenceForm from "../components/AddSentenceForm";
+
 const AddSentence = () => {
-  return <div>AddSentence</div>;
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center space-y-8 max-w-sm p-8 md:max-w-screen-lg mx-auto">
+        <h1 className="text-4xl font-bold">Cumle ekle</h1>
+        <AddSentenceForm />
+      </div>
+    </>
+  );
 };
 
 export default AddSentence;
