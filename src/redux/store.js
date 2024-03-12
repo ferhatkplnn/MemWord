2583ded78371f0fa5b8f906379e57d60df42e700
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-12 12:26:48 +0300

    feat: Add sentence reducer and slice.

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore, createListenerMiddleware } from "@reduxjs/toolkit";
 import wordsReducer from "./words/wordsSlice";
 import UIReducer from "./words/UISlice";
 import { saveWords } from "./listeners";
+import sentenceReducer from "./sentenct/sentenceSlice";
 
 const listenerMiddleware = createListenerMiddleware();
 listenerMiddleware.startListening(saveWords);
@@ -10,6 +11,7 @@ export const store = configureStore({
   reducer: {
     words: wordsReducer,
     UI: UIReducer,
+    sentence: sentenceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(listenerMiddleware.middleware),
