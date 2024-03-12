c96f5f034bdea5d5333047268dd2deb2e73e0abd
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-13 01:39:07 +0300

    feat: Add useAutoAnimate integration and ref to parent div in AddWord component
    - Integrate @formkit/auto-animate/react for animation effects
    - Add ref to parent div for animation control

--- src/pages/AddWord.jsx
+++ src/pages/AddWord.jsx
@@ -3,11 +3,16 @@ import { useSelector } from "react-redux";
 import { selectWordIds } from "../redux/words/wordsSlice";
 import EditWordForm from "../components/EditWordForm";
 import { useMemo } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+
 function AddWord() {
   const wordIds = useSelector(selectWordIds);
+  const [parent] = useAutoAnimate();
+
   const reversedWordIds = useMemo(() => {
     return [...wordIds].reverse();
   }, [wordIds]);
+
   return (
     <>
       <div className="flex flex-col justify-center items-center space-y-8 max-w-sm p-8 md:max-w-screen-lg mx-auto">
@@ -15,7 +20,7 @@ function AddWord() {
 
         <AddWordForm />
 
-        <div className="space-y-2 ">
+        <div ref={parent} className="space-y-2 ">
           {reversedWordIds.map((id) => (
             <EditWordForm key={id} id={id} />
           ))}
