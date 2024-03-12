920cc5708f5e02f8ebf50b806b5ba6fa1697c9a6
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-12 13:33:40 +0300

    feat(component): enhance AddSentence page with EditSentenceForm list
    - Added useSelector to retrieve sentenceIds from the Redux store
    - Utilized useMemo to create a reversedSentenceIds array for rendering
    - Integrated EditSentenceForm for each sentence in the list

--- src/pages/AddSentence.jsx
+++ src/pages/AddSentence.jsx
@@ -1,11 +1,27 @@
 import AddSentenceForm from "../components/AddSentenceForm";
+import { useSelector } from "react-redux";
+import { selectSentenceIds } from "../redux/sentence/sentenceSlice";
+import { useMemo } from "react";
+import { EditSentenceForm } from "../components/EditSentenceForm";
 
 const AddSentence = () => {
+  const sentenceIds = useSelector(selectSentenceIds);
+
+  const reversedSentenceIds = useMemo(() => {
+    return [...sentenceIds].reverse();
+  }, [sentenceIds]);
+
   return (
     <>
       <div className="flex flex-col justify-center items-center space-y-8 max-w-sm p-8 md:max-w-screen-lg mx-auto">
         <h1 className="text-4xl font-bold">Cumle ekle</h1>
         <AddSentenceForm />
+
+        <div className="space-y-2 ">
+          {reversedSentenceIds.map((id) => (
+            <EditSentenceForm key={id} id={id} />
+          ))}
+        </div>
       </div>
     </>
   );
